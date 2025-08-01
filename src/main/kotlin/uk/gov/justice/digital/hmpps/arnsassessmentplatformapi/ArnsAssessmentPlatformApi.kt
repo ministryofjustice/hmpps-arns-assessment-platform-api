80252647562d39fc855ab91c3c088d5f64a447e0
runner <runner@hmpps-github-actions-runner-75677c4c8f-hh2qc>
2025-08-01 13:31:36 +0000

    Rename project

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/ArnsAssessmentPlatformApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/ArnsAssessmentPlatformApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.arnsassessmentplatformapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class ArnsAssessmentPlatformApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<ArnsAssessmentPlatformApi>(*args)
 }
