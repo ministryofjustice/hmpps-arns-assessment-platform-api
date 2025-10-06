0b043c979b1149d705084044451c0eabfe3decda
Lucas Cairns <lucas.cairns@gmail.com>
2025-10-06 18:00:47 +0100

    Add additional unit tests for aggregates (#7)
    * Add additional unit tests for aggregates
    
    * Fix lint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/persistence/entity/aggregate/Aggregate.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/persistence/entity/aggregate/Aggregate.kt
@@ -3,12 +3,13 @@ package uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entit
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entity.EventEntity
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entity.event.Event
+import kotlin.reflect.KClass
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 sealed interface Aggregate {
   var numberOfEventsApplied: Long
   fun apply(event: EventEntity): Boolean
   fun clone(): Aggregate
-  fun shouldCreate(event: Event): Boolean
-  fun shouldUpdate(event: Event): Boolean
+  fun shouldCreate(event: KClass<out Event>): Boolean
+  fun shouldUpdate(event: KClass<out Event>): Boolean
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/persistence/entity/aggregate/AssessmentTimelineAggregate.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/persistence/entity/aggregate/AssessmentTimelineAggregate.kt
@@ -8,6 +8,7 @@ import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entity
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entity.event.AssessmentStatusUpdated
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entity.event.Event
 import java.time.LocalDateTime
+import kotlin.reflect.KClass
 
 private const val TYPE = "ASSESSMENT_TIMELINE"
 
@@ -19,9 +20,9 @@ data class TimelineItem(
 @JsonTypeName(TYPE)
 class AssessmentTimelineAggregate : Aggregate {
   private val timeline = mutableListOf<TimelineItem>()
-  private var previousStatus = ""
+  private var previousStatus: String? = null
 
-  fun handle(timestamp: LocalDateTime, event: AnswersUpdated) {
+  private fun handle(timestamp: LocalDateTime, event: AnswersUpdated) {
     timeline.add(
       TimelineItem(
         timestamp = timestamp,
@@ -31,7 +32,7 @@ class AssessmentTimelineAggregate : Aggregate {
     numberOfEventsApplied += 1
   }
 
-  fun handle(timestamp: LocalDateTime, event: AnswersRolledBack) {
+  private fun handle(timestamp: LocalDateTime, event: AnswersRolledBack) {
     timeline.add(
       TimelineItem(
         timestamp = timestamp,
@@ -41,13 +42,14 @@ class AssessmentTimelineAggregate : Aggregate {
     numberOfEventsApplied += 1
   }
 
-  fun handle(timestamp: LocalDateTime, event: AssessmentStatusUpdated) {
-    timeline.add(
-      TimelineItem(
-        timestamp = timestamp,
-        details = "Assessment status changed from \"$previousStatus\" to \"${event.status}\"",
-      ),
-    )
+  private fun handle(timestamp: LocalDateTime, event: AssessmentStatusUpdated) {
+    val details = if (!previousStatus.isNullOrBlank()) {
+      "Assessment status changed from \"$previousStatus\" to \"${event.status}\""
+    } else {
+      "Assessment status changed to \"${event.status}\""
+    }
+
+    timeline.add(TimelineItem(timestamp = timestamp, details = details))
     numberOfEventsApplied += 1
     previousStatus = event.status
   }
@@ -67,20 +69,20 @@ class AssessmentTimelineAggregate : Aggregate {
     return true
   }
 
-  override fun shouldCreate(event: Event) = createsOn.contains(event::class) || numberOfEventsApplied % 50L == 0L
+  override fun shouldCreate(event: KClass<out Event>) = createsOn.contains(event) || (numberOfEventsApplied > 1 && numberOfEventsApplied % 50L == 0L)
 
-  override fun shouldUpdate(event: Event) = updatesOn.contains(event::class)
+  override fun shouldUpdate(event: KClass<out Event>) = updatesOn.contains(event)
 
   override fun clone() = AssessmentTimelineAggregate()
     .also {
-      it.timeline.plus(timeline)
+      it.timeline.addAll(timeline)
       it.previousStatus = previousStatus
     }
 
   companion object : AggregateType {
     override val getInstance = { AssessmentTimelineAggregate() }
     override val aggregateType = TYPE
-    override val createsOn = setOf(AssessmentCreated::class)
-    override val updatesOn = setOf(AnswersUpdated::class, AnswersRolledBack::class, AssessmentStatusUpdated::class)
+    override val createsOn: Set<KClass<out Event>> = setOf(AssessmentCreated::class)
+    override val updatesOn: Set<KClass<out Event>> = setOf(AnswersUpdated::class, AnswersRolledBack::class, AssessmentStatusUpdated::class)
   }
 }
