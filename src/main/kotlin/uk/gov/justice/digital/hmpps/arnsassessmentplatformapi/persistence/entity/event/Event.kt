c50dc37d618edc76b07fdd6d024a07d350de02bc
Lucas Cairns <lucas.cairns@gmail.com>
2025-10-10 15:44:06 +0100

    Refactor aggregate response mappers
    - Refactor aggregate response mappers
    - Add coverage for event/command buses
    - Refacter dates used in testing to be more readable

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/persistence/entity/event/Event.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/persistence/entity/event/Event.kt
@@ -1,8 +1,6 @@
 package uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entity.event
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
-import kotlin.reflect.KClass
-import kotlin.reflect.full.companionObjectInstance
 
 interface EventType {
   val eventType: String
@@ -10,10 +8,3 @@ interface EventType {
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 sealed interface Event
-
-val <T : Event> KClass<T>.eventType: String
-  get() = (this.companionObjectInstance as? EventType)
-    ?.eventType ?: error("No EventCompanion on ${this.qualifiedName}")
-
-val Event.eventType: String
-  get() = this::class.eventType
