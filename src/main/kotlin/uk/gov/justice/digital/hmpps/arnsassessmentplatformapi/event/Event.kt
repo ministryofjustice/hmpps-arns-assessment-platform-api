24070ea50b6c832eeee74e70e85d4236919d2776
devkokov <dm.kokov@gmail.com>
2025-12-02 13:18:35 +0000

    Define JsonSubTypes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/event/Event.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/event/Event.kt
@@ -1,9 +1,24 @@
 package uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.event
 
+import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.command.Timeline
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.SIMPLE_NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes(
+  JsonSubTypes.Type(value = AssessmentAnswersRolledBackEvent::class, name = "AssessmentAnswersRolledBackEvent"),
+  JsonSubTypes.Type(value = AssessmentAnswersRolledBackEvent::class, name = "AssessmentAnswersRolledBackEvent"),
+  JsonSubTypes.Type(value = AssessmentAnswersUpdatedEvent::class, name = "AssessmentAnswersUpdatedEvent"),
+  JsonSubTypes.Type(value = AssessmentCreatedEvent::class, name = "AssessmentCreatedEvent"),
+  JsonSubTypes.Type(value = AssessmentPropertiesUpdatedEvent::class, name = "AssessmentPropertiesUpdatedEvent"),
+  JsonSubTypes.Type(value = CollectionCreatedEvent::class, name = "CollectionCreatedEvent"),
+  JsonSubTypes.Type(value = CollectionItemAddedEvent::class, name = "CollectionItemAddedEvent"),
+  JsonSubTypes.Type(value = CollectionItemAnswersUpdatedEvent::class, name = "CollectionItemAnswersUpdatedEvent"),
+  JsonSubTypes.Type(value = CollectionItemPropertiesUpdatedEvent::class, name = "CollectionItemPropertiesUpdatedEvent"),
+  JsonSubTypes.Type(value = CollectionItemRemovedEvent::class, name = "CollectionItemRemovedEvent"),
+  JsonSubTypes.Type(value = CollectionItemReorderedEvent::class, name = "CollectionItemReorderedEvent"),
+  JsonSubTypes.Type(value = FormVersionUpdatedEvent::class, name = "FormVersionUpdatedEvent"),
+)
 sealed interface Event {
   val timeline: Timeline?
 }
