c939871a61782275db4c6e6b2e5e97fd7c811c1b
devkokov <dm.kokov@gmail.com>
2025-12-10 15:02:57 +0000

    Add a Person entity (WIP)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/persistence/entity/PersonEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/persistence/entity/PersonEntity.kt
@@ -6,16 +6,15 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.config.Clock
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Entity
-@Table(name = "assessment")
-class AssessmentEntity(
+@Table(name = "person")
+class PersonEntity(
   @Id
   @Column(name = "id")
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,7 +26,9 @@ class AssessmentEntity(
   @Column(name = "created_at")
   val createdAt: LocalDateTime = Clock.now(),
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "person_uuid", referencedColumnName = "uuid", updatable = false, nullable = false)
-  val person: PersonEntity,
+  @Column(name = "crn", unique = true, nullable = false)
+  val crn: String,
+
+  @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
+  val assessments: MutableList<AssessmentEntity> = mutableListOf(),
 )
