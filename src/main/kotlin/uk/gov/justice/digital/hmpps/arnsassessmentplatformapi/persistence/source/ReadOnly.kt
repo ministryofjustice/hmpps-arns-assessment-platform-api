bc06bd681b6541dec21076289fd71e6d77795a26
devkokov <dm.kokov@gmail.com>
2025-12-31 14:18:03 +0000

    Remove read replica

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/persistence/source/ReadOnly.kt
+++
@@ -1,5 +0,0 @@
-package uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.source
-
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class ReadOnly
--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/query/bus/QueryBus.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/query/bus/QueryBus.kt
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.controller.response.QueriesResponse
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.controller.response.QueryResponse
-import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.source.ReadOnly
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.query.Query
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.query.RequestableQuery
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.service.AuditService
@@ -17,7 +16,6 @@ class QueryBus(
   fun dispatch(query: Query) = registry.getHandlerFor(query::class).execute(query)
     .also { if (query is RequestableQuery) auditService.audit(query) }
 
-  @ReadOnly
   @Transactional(readOnly = true)
   fun dispatch(queries: List<Query>) = QueriesResponse(queries.map { QueryResponse(it, dispatch(it)) })
 }
