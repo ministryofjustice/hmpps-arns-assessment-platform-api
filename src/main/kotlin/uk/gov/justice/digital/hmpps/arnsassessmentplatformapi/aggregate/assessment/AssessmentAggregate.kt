0ba22d21e5ab171d237a3f9e285bed1d74979858
Lucas Cairns <lucas.cairns@gmail.com>
2026-02-03 12:39:18 +0000

    Fix lint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/aggregate/assessment/AssessmentAggregate.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/aggregate/assessment/AssessmentAggregate.kt
@@ -40,14 +40,15 @@ class AssessmentAggregate :
       }
     }
 
-  override fun getCollectionWithItem(collectionItemUuid: UUID): Collection? =
-    collections.firstNotNullOfOrNull { collection ->
-      collection.items.firstNotNullOfOrNull { item ->
-        if (item.uuid == collectionItemUuid) collection
-        else item.findCollectionWithItem(collectionItemUuid)
+  override fun getCollectionWithItem(collectionItemUuid: UUID): Collection? = collections.firstNotNullOfOrNull { collection ->
+    collection.items.firstNotNullOfOrNull { item ->
+      if (item.uuid == collectionItemUuid) {
+        collection
+      } else {
+        item.findCollectionWithItem(collectionItemUuid)
       }
     }
+  }
 
-  override fun getCollectionItem(collectionItemUuid: UUID) =
-    collections.firstNotNullOfOrNull { it.findItem(collectionItemUuid) }
+  override fun getCollectionItem(collectionItemUuid: UUID) = collections.firstNotNullOfOrNull { it.findItem(collectionItemUuid) }
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/model/CollectionItem.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/model/CollectionItem.kt
@@ -29,12 +29,13 @@ data class CollectionItem(
   override fun findCollection(id: UUID): Collection? = collections.firstOrNull { it.uuid == id }
     ?: collections.firstNotNullOfOrNull { collection -> collection.items.firstNotNullOfOrNull { it.findCollection(id) } }
 
-  fun findCollectionWithItem(collectionItemUuid: UUID): Collection? {
-    return collections.firstNotNullOfOrNull { collection ->
-      val item = collection.items.firstOrNull { item ->  item.uuid === collectionItemUuid }
+  fun findCollectionWithItem(collectionItemUuid: UUID): Collection? = collections.firstNotNullOfOrNull { collection ->
+    val item = collection.items.firstOrNull { item -> item.uuid === collectionItemUuid }
 
-      if (item !== null) collection
-      else collection.items.firstNotNullOfOrNull { item -> item.findCollectionWithItem(collectionItemUuid) }
+    if (item !== null) {
+      collection
+    } else {
+      collection.items.firstNotNullOfOrNull { item -> item.findCollectionWithItem(collectionItemUuid) }
     }
   }
 }
