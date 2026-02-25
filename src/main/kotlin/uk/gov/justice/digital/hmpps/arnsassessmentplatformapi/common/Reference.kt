b8ab4a7904182e3fa0c47817f2cd47b5c1efd5c3
devkokov <dm.kokov@gmail.com>
2026-02-25 18:06:51 +0000

    fix failing tests (hopefully)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/common/Reference.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/common/Reference.kt
@@ -1,11 +1,13 @@
 package uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.common
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonValue
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.command.bus.getUuidAt
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.controller.response.CommandResponse
 import java.util.UUID
 
-data class Reference(
+data class Reference @JsonCreator(mode = JsonCreator.Mode.DELEGATING) constructor(
+  @get:JsonValue
   private val rawValue: String,
 ) {
   lateinit var value: UUID
@@ -19,12 +21,6 @@ data class Reference(
   }
 
   fun resolve() = resolve(emptyList())
-
-  companion object {
-    @JvmStatic
-    @JsonCreator
-    fun from(value: String) = Reference(value)
-  }
 }
 
 fun UUID.toReference() = Reference(this.toString()).also { it.resolve() }
