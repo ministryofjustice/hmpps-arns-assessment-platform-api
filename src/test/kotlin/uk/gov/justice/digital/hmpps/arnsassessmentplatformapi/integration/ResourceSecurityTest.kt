8a579cc304db9f12b45c785f00e4e7fd13e4d72e
devkokov <dm.kokov@gmail.com>
2026-03-12 16:19:46 +0000

    fix test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/integration/ResourceSecurityTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/integration/ResourceSecurityTest.kt
@@ -50,7 +50,7 @@ class ResourceSecurityTest : IntegrationTestBase() {
   }
 
   @Test
-  fun `Ensure command and query endpoints require ROLE_AAP__FRONTEND_RW or ROLE_AAP__COORDINATOR_RW`() {
+  fun `Ensure command and query endpoints require ROLE_AAP__FRONTEND_RW, ROLE_AAP__COORDINATOR_RW or ROLE_SENTENCE_PLAN_WRITE`() {
     val beans = context.getBeansOfType(RequestMappingHandlerMapping::class.java)
     beans.forEach { (_, mapping) ->
       mapping.handlerMethods.forEach { (mappingInfo, method) ->
@@ -66,8 +66,8 @@ class ResourceSecurityTest : IntegrationTestBase() {
           }.isNotNull()
 
           assertThat(annotation?.value).withFailMessage {
-            "Endpoint ${paths.first()} must require ROLE_AAP__FRONTEND_RW or ROLE_AAP__COORDINATOR_RW, but has: ${annotation?.value}"
-          }.isEqualTo("hasAnyRole('ROLE_AAP__FRONTEND_RW', 'ROLE_AAP__COORDINATOR_RW')")
+            "Endpoint ${paths.first()} must require ROLE_AAP__FRONTEND_RW, ROLE_AAP__COORDINATOR_RW or ROLE_SENTENCE_PLAN_WRITE, but has: ${annotation?.value}"
+          }.isEqualTo("hasAnyRole('ROLE_AAP__FRONTEND_RW', 'ROLE_AAP__COORDINATOR_RW', 'ROLE_SENTENCE_PLAN_WRITE')")
         }
       }
     }
