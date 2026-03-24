2c99efb42929b488e51df38ad2ddd4062d444f50
devkokov <dm.kokov@gmail.com>
2026-03-24 18:03:07 +0000

    fix integration tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/integration/IntegrationTestBase.kt
@@ -4,6 +4,7 @@ import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
@@ -18,7 +19,10 @@ import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.common.UserDetails
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.controller.request.CommandsRequest
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.controller.request.QueriesRequest
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.controller.response.CommandsResponse
+import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.integration.wiremock.HmppsAuthApiExtension
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.integration.wiremock.HmppsAuthApiExtension.Companion.hmppsAuth
+import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.UserDetailsRepository
+import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.cache.UserCache
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entity.AuthSource
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entity.UserDetailsEntity
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.query.RequestableQuery
@@ -26,7 +30,7 @@ import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.service.UserDetail
 import uk.gov.justice.hmpps.test.kotlin.auth.JwtAuthorisationHelper
 import java.time.LocalDateTime
 
-// @ExtendWith(HmppsAuthApiExtension::class)
+@ExtendWith(HmppsAuthApiExtension::class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("postgres", "test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -47,14 +51,14 @@ abstract class IntegrationTestBase {
   @BeforeAll
   fun setupWebTestClient() {
     every { clock.now() } answers { LocalDateTime.now() }
-    testUserDetailsEntity = userDetailsService.findOrCreate(testUserDetails)
+    testUserDetailsEntity = UserDetailsService(userDetailsRepository, UserCache()).findOrCreate(testUserDetails)
     webTestClient = WebTestClient.bindToServer()
       .baseUrl("http://localhost:$port")
       .build()
   }
 
   @Autowired
-  private lateinit var userDetailsService: UserDetailsService
+  private lateinit var userDetailsRepository: UserDetailsRepository
 
   @Autowired
   protected lateinit var jwtAuthHelper: JwtAuthorisationHelper
