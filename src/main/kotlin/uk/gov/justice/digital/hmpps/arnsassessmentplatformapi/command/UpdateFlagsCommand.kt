793cd1b0b954604056cc552f6e3bb82a09d45990
devkokov <dm.kokov@gmail.com>
2026-08-25 09:59:50 +0100

    add hooks to new UpdateFlagsCommand

--- src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/command/UpdateFlagsCommand.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/command/UpdateFlagsCommand.kt
@@ -2,10 +2,12 @@ package uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.command
 
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.common.Reference
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.common.UserDetails
+import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.hook.Hook
 
 data class UpdateFlagsCommand(
   override val user: UserDetails,
   override val assessmentUuid: Reference,
   val flags: List<String>,
   override val timeline: Timeline? = null,
+  override val hooks: List<Hook>? = null,
 ) : RequestableCommand
