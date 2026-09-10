07d6330d3425d16fbd1f606383e1ff7350e0392b
Kaustav Surai <kaustav.surai@justice.gov.uk>
2026-09-10 09:39:01 +0100

    PR comments

--- src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/e2e/AapAssessmentApiTest.kt
+++ src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/arnsassessmentplatformapi/e2e/AapAssessmentApiTest.kt
@@ -3,37 +3,36 @@ package uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.e2e
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.expectBody
+import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.command.CreateAssessmentCommand
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.command.result.CreateAssessmentCommandResult
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.common.UserDetails
+import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.controller.request.CommandsRequest
 import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.controller.response.CommandsResponse
-import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.e2e.dto.AssessmentRequestDto
-import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.e2e.dto.Command
-import java.util.*
+import uk.gov.justice.digital.hmpps.arnsassessmentplatformapi.persistence.entity.IdentifierType
+import java.util.UUID
 
-@DisplayName("AAP API Tests")
+@DisplayName("Create Assessment API Tests")
 class AapAssessmentApiTest : IntegrationTestBase() {
-  private val log = LoggerFactory.getLogger(this::class.java)
 
   @Test
   fun `create assessment`() {
-    val testUserDetails = UserDetails(id = "test-user", name = "Test User")
     val testCrn = UUID.randomUUID().toString()
-    val command = Command(
-      type = "CreateAssessmentCommand",
-      assessmentType = "Test",
-      formVersion = "1.0",
-      properties = emptyMap(),
-      user = testUserDetails,
-      CRN = testCrn,
-      flags = listOf("SAN_BETA"),
+    val assessmentCommandRequest = CommandsRequest(
+      commands = listOf(
+        CreateAssessmentCommand(
+          UserDetails("test-user", "Test User"),
+          assessmentType = "TEST",
+          formVersion = "1.0",
+          identifiers = mapOf(IdentifierType.CRN to testCrn),
+          flags = listOf("SAN_BETA"),
+        ),
+      ),
     )
-    val assessmentRequestDto = AssessmentRequestDto(commands = listOf(command))
 
     val commandsResponse = webTestClient.post().uri("/command")
-      .bodyValue(assessmentRequestDto)
+      .bodyValue(assessmentCommandRequest)
       .accept(MediaType.APPLICATION_JSON)
       .exchange()
       .expectStatus().isOk
